6a031ae68d5ae9e187ad47d2e51411b9c5f28ca7
Taylor Otwell <taylorotwell@gmail.com>
2012-06-04 22:32:49 -0500

    more tests.

--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -18,6 +18,21 @@ class ConnectionTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('foo', $connection->getQueryGrammar());
 	}
 
+
+	public function testSelectOneCallsSelectAndReturnsSingleResult()
+	{
+		$connection = $this->getMockConnection(array('select'));
+		$connection->expects($this->once())->method('select')->with('foo', array('bar' => 'baz'))->will($this->returnValue(array('foo')));
+		$this->assertEquals('foo', $connection->selectOne('foo', array('bar' => 'baz')));
+	}
+
+
+	protected function getMockConnection($methods = array(), $pdo = null)
+	{
+		$pdo = $pdo ?: new MockPDO;
+		return $this->getMock('Illuminate\Database\Connection', array_merge(array('getDefaultQueryGrammar'), $methods), array($pdo));
+	}
+
 }
 
 class MockPDO extends PDO { public function __construct() {} }
