4b8e8b90fc4e4453a5cdccebcb648665a9b93d77
Taylor Otwell <taylorotwell@gmail.com>
2012-07-05 16:42:29 -0500

    work on eager loading.

--- src/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
+++ src/Illuminate/Database/Eloquent/Relations/BelongsToMany.php
@@ -235,7 +235,7 @@ class BelongsToMany extends Relation {
 		{
 			$values = $this->cleanPivotAttributes($model);
 
-			$pivot = $this->createPivot($values);
+			$pivot = $this->newExistingPivot($values);
 
 			$model->setRelation('pivot', $pivot);
 		}
