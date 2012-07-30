ded91d0f78397cee4dc57588877ef68972708fd0
Taylor Otwell <taylorotwell@gmail.com>
2012-07-30 01:04:37 -0500

    work on mysql schema grammar.

--- src/Illuminate/Database/Schema/Grammars/Grammar.php
+++ src/Illuminate/Database/Schema/Grammars/Grammar.php
@@ -7,6 +7,43 @@ use Illuminate\Database\Grammar as BaseGrammar;
 abstract class Grammar extends BaseGrammar {
 
 	/**
+	 * Compile a foreign key command.
+	 *
+	 * @param  Illuminate\Database\Schema\Blueprint  $blueprint
+	 * @param  Illuminate\Support\Fluent  $command
+	 * @return string
+	 */
+	public function compileForeign(Blueprint $blueprint, Fluent $command)
+	{
+		$table = $this->wrapTable($blueprint);
+
+		$on = $this->wrapTable($command->on);
+
+		$columns = $this->columnize($command->columns);
+
+		$onColumns = $this->columnize($command->references);
+
+		$sql = "alter table {$table} add constraint {$command->name} ";
+
+		$sql .= "foreign key {$columns} references {$on} ({$onColumns})";
+
+		// Once we have the basic foreign key creation statement constructed we can
+		// build out the syntax for what should happen on an update or delete of
+		// the affected columns, which will get something like "cascade", etc.
+		if ( ! is_null($command->onDelete))
+		{
+			$sql .= " on delete {$command->onDelete}";
+		}
+
+		if ( ! is_null($command->onUpdate))
+		{
+			$sql .= " on update {$command->onUpdate}";
+		}
+
+		return $sql;
+	}
+
+	/**
 	 * Get the SQL for the column data type.
 	 *
 	 * @param  Illuminate\Support\Fluent  $column
