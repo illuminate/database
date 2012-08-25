e336c6350bb63741825ed70c7df35d0664db6f49
Taylor Otwell <taylorotwell@gmail.com>
2012-08-25 13:12:46 -0500

    added drop column tests.

--- tests/MySqlSchemaGrammarTest.php
+++ tests/MySqlSchemaGrammarTest.php
@@ -37,6 +37,35 @@ class MySqlSchemaGrammarTest extends PHPUnit_Framework_TestCase {
 	}
 
 
+	public function testDropColumn()
+	{
+		$blueprint = new Blueprint('users');
+		$blueprint->dropColumn('foo');
+		$statements = $blueprint->toSql($this->getGrammar());
+
+		$this->assertEquals(1, count($statements));
+		$this->assertEquals('alter table `users` drop `foo`', $statements[0]);
+
+		$blueprint = new Blueprint('users');
+		$blueprint->dropColumn(array('foo', 'bar'));
+		$statements = $blueprint->toSql($this->getGrammar());
+
+		$this->assertEquals(1, count($statements));
+		$this->assertEquals('alter table `users` drop `foo`, drop `bar`', $statements[0]);
+	}
+
+
+	public function testDropColumns()
+	{
+		$blueprint = new Blueprint('users');
+		$blueprint->dropColumns('foo', 'bar');
+		$statements = $blueprint->toSql($this->getGrammar());
+
+		$this->assertEquals(1, count($statements));
+		$this->assertEquals('alter table `users` drop `foo`, drop `bar`', $statements[0]);
+	}
+
+
 	protected function getConnection()
 	{
 		return m::mock('Illuminate\Database\Connection');
