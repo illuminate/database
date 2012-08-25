1dbd40205cce7eef464f2a2d41100481d48cca3d
Taylor Otwell <taylorotwell@gmail.com>
2012-08-25 13:08:29 -0500

    adding tests.

--- tests/MySqlSchemaGrammarTest.php
+++ tests/MySqlSchemaGrammarTest.php
@@ -15,6 +15,14 @@ class MySqlSchemaGrammarTest extends PHPUnit_Framework_TestCase {
 
 		$this->assertEquals(1, count($statements));
 		$this->assertEquals('create table `users` (`id` int not null auto_increment primary key, `email` varchar(255) not null)', $statements[0]);
+
+		$blueprint = new Blueprint('users');
+		$blueprint->increments('id');
+		$blueprint->string('email');
+		$statements = $blueprint->toSql($this->getGrammar());
+
+		$this->assertEquals(1, count($statements));
+		$this->assertEquals('alter table `users` add `id` int not null auto_increment primary key, add `email` varchar(255) not null', $statements[0]);
 	}
 
 
