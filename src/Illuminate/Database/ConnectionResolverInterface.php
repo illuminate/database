c03244b5b2abcab3269e93729d02006f4422e428
Taylor Otwell <taylorotwell@gmail.com>
2012-10-25 22:13:08 -0500

    Add a ConnectionResolverInterface contract.

---
+++ src/Illuminate/Database/ConnectionResolverInterface.php
@@ -0,0 +1,13 @@
+<?php namespace Illuminate\Database;
+
+interface ConnectionResolverInterface {
+
+	/**
+	 * Get a database connection instance.
+	 *
+	 * @param  string  $name
+	 * @return Illuminate\Database\Connection
+	 */
+	public function connection($name = null);
+
+}
