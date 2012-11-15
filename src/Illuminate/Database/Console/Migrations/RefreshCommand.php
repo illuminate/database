da49e93cffa4f9ca56346a04b32d3ae9ec171d2b
Taylor Otwell <taylorotwell@gmail.com>
2012-11-15 16:42:30 -0600

    added refresh command.

---
+++ src/Illuminate/Database/Console/Migrations/RefreshCommand.php
@@ -0,0 +1,55 @@
+<?php namespace Illuminate\Database\Console\Migrations;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+
+class RefreshCommand extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'migrate:refresh';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Reset and re-run all migrations';
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return void
+	 */
+	public function fire()
+	{
+		$database = $this->getOption('database');
+
+		$this->call('migrate:reset', array('--database' => $database));
+
+		$this->call('migrate', array('--database' => $database));
+
+		if ($this->getOption('seed'))
+		{
+			$this->call('db:seed', array('--database' => $database));
+		}
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return array(
+			array('database', null, InputOption::VALUE_OPTIONAL, 'The database connection to use'),
+
+			array('seed', null, InputOption::VALUE_NONE, 'Indicates if the seed task should be re-run'),
+		);
+	}
+
+}
