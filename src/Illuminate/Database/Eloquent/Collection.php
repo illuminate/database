0737151bfc2131710d29ebba39f711d963dbb4ba
Taylor Otwell <taylorotwell@gmail.com>
2012-11-26 20:23:49 -0600

    added ->load method to collection.

--- src/Illuminate/Database/Eloquent/Collection.php
+++ src/Illuminate/Database/Eloquent/Collection.php
@@ -27,6 +27,22 @@ class Collection implements ArrayAccess, ArrayableInterface, Countable, Iterator
 	}
 
 	/**
+	 * Load a set of relationships onto the collection.
+	 *
+	 * @param  dynamic  string
+	 * @return void
+	 */
+	public function load()
+	{
+		if (count($items) > 0)
+		{
+			$query = $this->items->first()->newQuery()->with(func_get_args());
+
+			$this->items = $query->eagerLoadRelations($this->items);
+		}
+	}
+
+	/**
 	 * Add an item to the collection.
 	 *
 	 * @param  mixed  $item
