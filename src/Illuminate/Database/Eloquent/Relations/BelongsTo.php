43768faa91dd68d67ed5e9ec67954d8fd44adaf5
Taylor Otwell <taylorotwell@gmail.com>
2013-01-02 21:56:08 -0600

    fix bug with invalid query on belongs to eager load.

--- src/Illuminate/Database/Eloquent/Relations/BelongsTo.php
+++ src/Illuminate/Database/Eloquent/Relations/BelongsTo.php
@@ -79,6 +79,8 @@ class BelongsTo extends Relation {
 	{
 		$keys = array();
 
+		if (count($models) == 0) return array(0);
+
 		// First we need to gather all of the keys from the parent models so we know what
 		// to query for via the eager loading query. We will add them to an array then
 		// execute a "where in" statement to gather up all of those related records.
