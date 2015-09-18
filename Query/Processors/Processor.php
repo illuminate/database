9d5e88e1ebfa9f1e9ae219cfb98589757d8094cb
Max Kovpak <max_kovpak@hotmail.com>
2015-09-18 15:37:18 +0300

    Added index by column ability

--- Query/Processors/Processor.php
+++ Query/Processors/Processor.php
@@ -10,11 +10,43 @@ class Processor
      * Process the results of a "select" query.
      *
      * @param  \Illuminate\Database\Query\Builder  $query
-     * @param  array  $results
+     * @param  array   $results
+     * @param  string  $indexBy
      * @return array
      */
-    public function processSelect(Builder $query, $results)
+    public function processSelect(Builder $query, $results, $indexBy = null)
     {
+        if($indexBy !== null && count($results)) {
+            $results = $this->indexResultsByColumn($results, $indexBy);
+        }
+
+        return $results;
+    }
+
+    /**
+     * Index results by column
+     *
+     * @param array $results
+     * @param string $column
+     * @return array
+     * */
+    protected function indexResultsByColumn(array $results, $column)
+    {
+        if($column === null) {
+            return $results;
+        }
+
+        $tmp = [];
+        foreach ($results as $row) {
+            if(is_object($row)) {
+                $tmp[$row->{$column}] = $row;
+            } else {
+                $tmp[$row[$column]] = $row;
+            }
+        }
+        $results = $tmp;
+        unset($tmp);
+
         return $results;
     }
 
