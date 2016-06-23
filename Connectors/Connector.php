7da5e67f052008abc791f4d9ed52aa17e5e9ed82
Brant Messenger <brantmessenger@users.noreply.github.com>
2016-06-23 15:06:24 -0500

    Update Connectors/Connector.php
    Create a convention to that uses configuration key 'password_encrypted' which would contain an encrypted password generated from Encrypter class. This would reduce the risk of exposing a decrypted password if the object or configuration values were dumped.

--- Connectors/Connector.php
+++ Connectors/Connector.php
@@ -6,6 +6,7 @@ use PDO;
 use Exception;
 use Illuminate\Support\Arr;
 use Illuminate\Database\DetectsLostConnections;
+use Illuminate\Encryption\Encrypter;
 
 class Connector
 {
@@ -43,19 +44,27 @@ class Connector
      * @param  string  $dsn
      * @param  array   $config
      * @param  array   $options
+     * @param  array   $decrypt
      * @return \PDO
      */
-    public function createConnection($dsn, array $config, array $options)
+    public function createConnection($dsn, array $config, array $options, $decrypt = null)
     {
         $username = Arr::get($config, 'username');
 
-        $password = Arr::get($config, 'password');
+        if (!is_null($decrypt)) {
+            $passwordEncrypted = Arr::get($config, 'password_encrypted');
+            $encrypter = new Encrypter($decrypt);
+            $password = $encrypter->decrypt($passwordEncrypted);
+            unset($encrypter);
+        } else {
+            $password = Arr::get($config, 'password');
+        }
 
         try {
             $pdo = new PDO($dsn, $username, $password, $options);
         } catch (Exception $e) {
             $pdo = $this->tryAgainIfCausedByLostConnection(
-                $e, $dsn, $username, $password, $options
+                $e, $dsn, $username, (!is_null($decrypt) ? $passwordEncrypted : $password, $options
             );
         }
 
