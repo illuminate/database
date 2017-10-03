f6f60aafd165663bd2ebae3371fd979dd1e6f7fa
Jhordan Lima <jhorzyto@gmail.com>
2017-10-03 10:37:43 -0300

    Added the ability to create a date instance when it does not meet the default format or default format. Very used to create dates coming from javascript objects by json.
    Example: 2017-10-03T03:00:00.000Z

--- Eloquent/Model.php
+++ Eloquent/Model.php
@@ -6,6 +6,7 @@ use DateTime;
 use Exception;
 use ArrayAccess;
 use Carbon\Carbon;
+use InvalidArgumentException;
 use LogicException;
 use JsonSerializable;
 use Illuminate\Support\Arr;
@@ -2909,10 +2910,19 @@ abstract class Model implements ArrayAccess, Arrayable, Jsonable, JsonSerializab
             return Carbon::createFromFormat('Y-m-d', $value)->startOfDay();
         }
 
-        // Finally, we will just assume this date is in the format used by default on
-        // the database connection and use that format to create the Carbon object
-        // that is returned back out to the developers after we convert it here.
-        return Carbon::createFromFormat($this->getDateFormat(), $value);
+        try {
+
+            // Finally, we will just assume this date is in the format used by default on
+            // the database connection and use that format to create the Carbon object
+            // that is returned back out to the developers after we convert it here.
+            return Carbon::createFromFormat(
+                $this->getDateFormat(), $value
+            );
+        } catch (InvalidArgumentException $e) {
+            // However if the submitted date is not in the default or defined format,
+            // eloquent will attempt to create from Carbon::parse.
+            return Carbon::parse($value);
+        }
     }
 
     /**
