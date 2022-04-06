b693a2ba91bd0b1f3ed23dfb15f6109f6fee4205
Thomas di Luccio <thomas.diluccio@platform.sh>
2022-04-06 17:20:20 +0200

    Add db:schema command to list a table  names and types

---
+++ Console/DbSchemaCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Illuminate\Database\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class DbSchemaCommand extends Command
+{
+    protected $signature = 'db:schema {table_name : The table whose column names and type should be listed}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get all the column names and types for a table';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $tableName = $this->argument('table_name');
+
+        $this->info('Column names and types for the table: ' . $tableName);
+
+        $schemaBuilder = DB::getSchemaBuilder();
+        $data = [];
+
+        foreach ($schemaBuilder->getColumnListing($tableName) as $columnName) {
+            $data[] = [
+                'name' => $columnName,
+                'type' => $schemaBuilder->getColumnType($tableName, $columnName),
+            ];
+        }
+
+        $headers = ['Column Name', 'Type'];
+        $this->table($headers, $data);
+    }
+}
