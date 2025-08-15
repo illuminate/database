262f27c15c8c155d3d8bf22bf36cc74685127fc4
Paulo Henrique Garcia <paulohenriquesg@gmail.com>
2025-08-15 06:58:57 +0200

    Add bool return, as it may happen
    In the incrementOrDecrement method, there is a flow that returns false. This is not reflected in the docblock.

--- Eloquent/Model.php
+++ Eloquent/Model.php
@@ -1006,7 +1006,7 @@ abstract class Model implements Arrayable, ArrayAccess, CanBeEscapedWhenCastToSt
      * @param  string  $column
      * @param  float|int  $amount
      * @param  array  $extra
-     * @return int
+     * @return int|bool
      */
     protected function increment($column, $amount = 1, array $extra = [])
     {
@@ -1019,7 +1019,7 @@ abstract class Model implements Arrayable, ArrayAccess, CanBeEscapedWhenCastToSt
      * @param  string  $column
      * @param  float|int  $amount
      * @param  array  $extra
-     * @return int
+     * @return int|bool
      */
     protected function decrement($column, $amount = 1, array $extra = [])
     {
@@ -1033,7 +1033,7 @@ abstract class Model implements Arrayable, ArrayAccess, CanBeEscapedWhenCastToSt
      * @param  float|int  $amount
      * @param  array  $extra
      * @param  string  $method
-     * @return int
+     * @return int|bool
      */
     protected function incrementOrDecrement($column, $amount, $extra, $method)
     {
